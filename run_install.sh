ef2a89840d3357b14273e9cc0c27c9fb426b513c
Matthew Brett <matthew.brett@gmail.com>
2014-07-05 02:28:01 +0100

    RF: remove cruft from install file

--- run_install.sh
+++ run_install.sh
@@ -6,12 +6,11 @@ FT_VERSION=2.5.3
 PNG_VERSION=1.6.12
 ZLIB_VERSION=1.2.8
 
-# Need cmake for openjpeg
-brew install cmake
 # Need pkg-config for freetype to find libpng
 brew install pkg-config
 # Set up build
 clean_builds
+clean_submodule matplotlib
 # Need zlib for compatibility with new libpng on OSX 10.6
 standard_install zlib $ZLIB_VERSION .tar.xz
 standard_install libpng $PNG_VERSION
