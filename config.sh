7a959283d7813196cdfd9e12484b8a8adc83ae1d
Matthew Brett <matthew.brett@gmail.com>
2016-06-17 11:44:04 -0700

    Refactor to multibuild

---
+++ config.sh
@@ -0,0 +1,38 @@
+# Define custom utilities
+IS_OSX=$([ "$TRAVIS_OS_NAME" == "osx" ] && echo true)
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    if [ -n "$IS_OSX" ]; then
+        export CC=clang
+        export CXX=clang++
+        brew install pkg-config
+    fi
+    # Use local freetype for versions which support it
+    export MPLLOCALFREETYPE=1
+    source multibuild/docker_lib_builders.sh
+    build_jpeg
+    build_libpng
+    build_bzip2
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    MPL_SRC_DIR=../matplotlib
+    # Get test images
+    MPL_INSTALL_DIR=$(dirname $(python -c 'import matplotlib; print(matplotlib.__file__)'))
+    cp -r ${MPL_SRC_DIR}/lib/matplotlib/tests/baseline_images $MPL_INSTALL_DIR/tests
+
+    echo "sanity checks"
+    python -c "import dateutil; print(dateutil.__version__)"
+    python -c "import sys; print('\n'.join(sys.path))"
+    python -c "import matplotlib; print(matplotlib.__file__)"
+    python -c "from matplotlib import font_manager"
+
+    echo "testing matplotlib using 1 process"
+    python $MPL_SRC_DIR/tests.py -sv
+
+    echo "Check import of tcl / tk"
+    MPLBACKEND="tkagg" python -c 'import matplotlib.pyplot as plt; print(plt.get_backend())'
+}
